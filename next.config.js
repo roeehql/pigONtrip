8b27294f351fe01c27beba99c8af2966ae1cbbcb
taei <kangnimell@gmail.com>
2022-12-28 02:55:50 +0900

    :construction_worker:  feat: config action 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const ON_GITHUB_PAGES = process.env.NODE_ENV === "production";
+
+const repoName = "pig-on-trip";
+
+const basePath = ON_GITHUB_PAGES ? `/${repoName}` : "";
+const assetPrefix = ON_GITHUB_PAGES ? `/${repoName}/` : "";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "akamai",
     path: "",
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
+  basePath,
+  assetPrefix,
 };
 
 module.exports = nextConfig;
