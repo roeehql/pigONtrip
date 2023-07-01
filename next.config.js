311524022986342d6c760e114d548908aee63cd8
taei <roeehql@naver.com>
2023-07-01 22:25:12 +0900

    chore: 미사용 패키지, 파일 삭제, config 수정

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
 
-const ON_GITHUB_PAGES = process.env.NODE_ENV === "production";
-
-const repoName = "pig-on-trip";
-
-const basePath = ON_GITHUB_PAGES ? `/${repoName}` : "";
-const assetPrefix = ON_GITHUB_PAGES ? `/${repoName}/` : "";
-
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-  reactStrictMode: true,
-  concurrentFeatures: true,
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/pigONtrip",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
   images: {
+    unoptimized: true,
     loader: "akamai",
-    path: "",
-  },
-  basePath,
-  assetPrefix,
-  env: {
-    BASE_URL: process.env.BASE_URL,
   },
 };
 
